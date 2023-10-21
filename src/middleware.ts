d259e3b5a574d8222da5b60403eabb04440e7ecb
German David <germanmarinrolong@gmail.com>
2023-10-21 14:06:18 -0500

    tasks route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/", "/profile", "/users", "/dashboard"] };
+export const config = { matcher: ["/", "/profile", "/users", "/tasks"] };
