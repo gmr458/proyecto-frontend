845a81f909f2b32defa48d37067e2c6a37e35210
German David <germanmarinrolong@gmail.com>
2023-10-24 00:50:39 -0500

    remove old commedted code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,37 +9,3 @@ export default function LoginPage() {
         </div>
     );
 }
-
-// export default function LoginPage() {
-//     const router = useRouter();
-
-//     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-//         event.preventDefault();
-
-//         const formData = new FormData(event.currentTarget);
-
-//         const response = await signIn("credentials", {
-//             email: formData.get("email"),
-//             password: formData.get("password"),
-//             redirect: false,
-//         });
-
-//         if (response?.error) {
-//             console.log({ response });
-//         }
-
-//         if (response?.ok) {
-//             return router.push("/profile");
-//         }
-//     };
-
-//     return (
-//         <div>
-//             <form onSubmit={handleSubmit}>
-//                 <input type="email" name="email" placeholder="email" />
-//                 <input type="password" name="password" placeholder="password" />
-//                 <button type="submit">SignIn</button>
-//             </form>
-//         </div>
-//     );
-// }
