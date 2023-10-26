b8f9415df14b4369726b2f5ea05f045f5b0371e2
German David <germanmarinrolong@gmail.com>
2023-10-26 14:39:21 -0500

    proteger ruta login despues de autenticacion

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,24 @@
-export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export const config = { matcher: ["/", "/profile", "/users", "/tasks"] };
+export default async function middleware(
+    req: NextRequest,
+    event: NextFetchEvent,
+) {
+    const token = await getToken({ req });
+    const isAuthenticated = !!token;
+
+    if (req.nextUrl.pathname.startsWith("/login") && isAuthenticated) {
+        return NextResponse.redirect(new URL("/profile", req.url));
+    }
+
+    const authMiddleware = withAuth({
+        pages: {
+            signIn: `/login`,
+        },
+    });
+
+    // @ts-expect-error
+    return authMiddleware(req, event);
+}
