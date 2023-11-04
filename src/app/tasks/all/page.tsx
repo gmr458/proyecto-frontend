f1f2d64d4546f0be5b60320aa6fbfdf72580a154
German David <germanmarinrolong@gmail.com>
2023-11-03 22:34:04 -0500

    remove greeting

--- src/app/tasks/all/page.tsx
+++ src/app/tasks/all/page.tsx
@@ -12,12 +12,6 @@ export default function AllTasksPage() {
 
     return (
         <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-            <div className="flex items-center justify-between space-y-2">
-                <div>
-                    <h2 className="text-2xl font-bold tracking-tight">Bienvenido</h2>
-                    <p className="text-mutes-foreground">Estas son todas la tareas creadas</p>
-                </div>
-            </div>
             <DataTable columns={columns} data={data} />
         </div>
     );
